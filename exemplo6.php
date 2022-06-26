595562e862bd133d70abadfbca912933f34e5802
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-06-26 20:02:44 -0300

    inserindo arquivos

---
+++ exemplo6.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constantes</title>
+</head>
+<body>
+<?php
+define ("meunome", "Thiago");
+define ("peso", 78);
+echo "O meu nome é ". meunome;   
+echo "Meu peso é ". peso;
+echo "<br>";
+
+$x = "tri";
+echo "O São Paulo é ${x}color";
+echo "<br>";
+echo "O São Paulo é ". $x ."color";
+echo "<br>";
+
+
+?>   
+</body>
+</html>
